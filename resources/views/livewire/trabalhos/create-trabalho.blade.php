6ad9e02c6a3c17a65bf6f0ea540d9e6bd82b5d40
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-10-22 09:01:40 -0300

    muda margem

--- resources/views/livewire/trabalhos/create-trabalho.blade.php
+++ resources/views/livewire/trabalhos/create-trabalho.blade.php
@@ -1,12 +1,12 @@
 <div>
     <form wire:submit="create">
-        <div class="my-6">
-            {{ $this->form }}
-        </div>
+        {{ $this->form }}
 
-        <button type="submit" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 dark:bg-custom-500 dark:hover:bg-custom-400 focus:ring-custom-500/50 dark:focus:ring-custom-400/50 fi-ac-btn-action">
-            Submit
-        </button>
+        <div class="my-5">
+            <button type="submit" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 dark:bg-custom-500 dark:hover:bg-custom-400 focus:ring-custom-500/50 dark:focus:ring-custom-400/50 fi-ac-btn-action">
+                Submit
+            </button>
+        </div>
     </form>
 
     <x-filament-actions::modals />
